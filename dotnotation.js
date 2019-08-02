500917150bc4ab51691303cc2d03ee71609bbc9e
codenation <codenation@codenations-MacBook-Pro.local>
2019-08-02 16:25:03 +0100

    first commit

---
+++ dotnotation.js
@@ -0,0 +1,24 @@
+/*
+
+//console.log("Hello".length);
+//length counts the characters in a string
+console.log("Hello".toUpperCase());
+//toUpperCase changes the string to all upper case let
+
+console.log("Hello")
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10));
+
+*/
+
+console.log("  |-----|");
+console.log("  |-----|");   
+console.log("  |-----|");
+console.log("----------");
+console.log("  |-----|");   
+console.log("  |-----|");
+console.log("  |-----|");
+console.log("----------");   
+console.log("  |-----|");
+console.log("  |-----|");
+console.log("  |-----|");   
---
+++ variables.js
@@ -0,0 +1,41 @@
+/*
+console.log("All around the world".toUpperCase().chartAt(7)
+let favouriteDrink = "coffe"
+console.log(favouriteDrink);
+console.log("My favourite drink is " + favouriteDrink);
+*/
+let myName = "Manuel Dias"
+let myAge = "43"
+let favColour = "black"
+console.log(`My name is ${myName}, I am ${myAge} years old, and my favourite colour is ${favColour}.`)
+
+//
+
+let space1 = " ";
+let space2 = "x";
+let space3 = "x";
+let space4 = "x";
+let space5 = "x";
+let space6 = " ";
+let space7 = "o";
+let space8 = " ";
+let space9 = " ";
+
+console.log("  |     |          " )
+console.log(` ${space1}  |  ${space2}   |  ${space3}`);
+console.log("  |     |           ");
+console.log("--------------------");
+console.log("  |     |           ");
+console.log(` ${space4}   |   ${space5} |  ${space6}`);
+console.log("  |     |           ");
+console.log("--------------------");
+console.log(` ${space7}   |   ${space8} |  ${space9}`);
+console.log("  |     |           ");
+if (space1 == "x" $space2 === "x" && $space3 ==="0");
+
+{
+    console.log("X WINNER!!!");
+} 
+ else {
+     console.log("try again");
+}     
